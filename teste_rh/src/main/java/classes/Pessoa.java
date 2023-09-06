ede921781b9da89c647a275f6ad278a5d89bfe76
João Vitor Miguez da Cunha <PET0147543@aluno.firjansenaisesi.com.br>
2023-09-06 11:06:37 -0300

    Creating enum class(Cargo)

--- teste_rh/src/main/java/classes/Pessoa.java
+++ teste_rh/src/main/java/classes/Pessoa.java
@@ -1,5 +1,35 @@
 package classes;
 
-abstract class Pessoa {
-    
+public abstract class Pessoa {
+    private String nome;
+    private int cpf;
+    private Endereco endereco;
+
+    //Constructor
+    public Pessoa(String nome, int cpf, Endereco endereco) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.endereco = endereco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getCpf() {
+        return cpf;
+    }
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+    public Endereco getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+
 }
