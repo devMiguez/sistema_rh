9443df1bb9abfa46c90d8006fb20798315a4450e
João Vitor Miguez da Cunha <PET0147543@aluno.firjansenaisesi.com.br>
2023-09-06 10:52:44 -0300

    First commit

---
+++ teste_rh/src/main/java/com/sistemarh/teste_rh/TesteRhApplication.java
@@ -0,0 +1,13 @@
+package com.sistemarh.teste_rh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteRhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteRhApplication.class, args);
+	}
+
+}
